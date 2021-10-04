2b83ff872353eb04b1704b34b0c10dad462884bb
rania <raniaarbash@hotmail.com>
2021-10-04 17:52:13 -0400

    * .gitignore: F
    * F21_NetworkingApp.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * NetworkingManager.cs:
    * Entitlements.plist:
    * MainActivity.cs:
    * F21_NetworkingApp.csproj:
    * Default.png:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * F21_NetworkingApp.iOS.csproj:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * F21_NetworkingApp.Android.csproj:
    * icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon80.png:
    * Icon76.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon29.png:
    * Icon20.png:
    * Icon120.png:
    * Icon152.png:
    * Icon167.png:
    * icon_round.xml:
    * Icon180.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ F21_NetworkingApp/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace F21_NetworkingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        String query;
+        ObservableCollection<IngredientClass> ingredients;
+        NetworkingManager manager = new NetworkingManager();
+        public MainPage()
+        {
+            InitializeComponent();
+           // 
+
+        }
+
+
+        protected  override void OnAppearing()
+        {
+            base.OnAppearing();
+            
+        }
+
+        async void  SearchBar_SearchButtonPressed(System.Object sender,
+            System.EventArgs e)
+        {
+
+           var listFromAPI = await manager.getInredient(query);
+           ingredients = new ObservableCollection<IngredientClass>();
+            foreach (IngredientClass i in listFromAPI)
+            {
+                var imageURL = "https://spoonacular.com/cdn/ingredients_100x100/" + i.image;
+                ingredients.Add(new IngredientClass(i.id,i.name,i.image,imageURL));
+            }
+            ingredintList.ItemsSource = ingredients;
+
+        }
+
+
+        void SearchBar_TextChanged(System.Object sender,
+            Xamarin.Forms.TextChangedEventArgs e)
+        {
+           query = e.NewTextValue;
+        }
+
+
+    }
+}
---
+++ F21_NetworkingApp/NetworkingManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace F21_NetworkingApp
+{
+    public class NetworkingManager
+    {
+
+        private string url = "https://api.spoonacular.com";
+        private string key = "8116c82ba63a49ba86bd5145aa25b245";
+        private string restOfURL1 = "/food/ingredients/search?query=";
+        private string restOfURL2 = "&apiKey=";
+
+
+        HttpClient client = new HttpClient();
+        public NetworkingManager()
+        {
+        }
+
+
+        public async Task<List<IngredientClass>> getInredient(string query)
+        {
+            string completeURL = url + restOfURL1 + query + restOfURL2 + key;
+            var response = await client.GetAsync(completeURL);
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return new List<IngredientClass>();
+            }
+            else
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var dic = JsonConvert.DeserializeObject<Dictionary<string, object>>
+                    (jsonString);
+                var array = dic.ElementAt(0).Value;
+                var finalList = JsonConvert.DeserializeObject<List<IngredientClass>>
+                     (array.ToString());
+                return finalList;
+            }
+        }
+    }
+}
